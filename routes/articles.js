9128b9d3154d291e3b91da64f2c6ec0bfc0479b7
Matheus Passos <msena789@gmail.com>
2021-01-25 13:20:17 -0300

    created model, form of article and save data in database

--- routes/articles.js
+++ routes/articles.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const Article = require('../models/article');
+
+router.get('/new', (req, res) => res.render('articles/new', { article: new Article() }));
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+    });
+
+    try {
+        article = await article.save();
+        res.redirect(`/articles/${article.id}`);
+    } catch (e) {
+        res.render('articles/new', { article: article })
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
 
 const app = express();
 
+mongoose.connect('', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }));
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
