63f3481a4bd59f856356f72fc4e55c6767c1b2fc
Matheus Passos <msena789@gmail.com>
2021-01-25 13:29:18 -0300

    listing articles in database, create page show details article

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
 
 const app = express();
 
@@ -14,8 +15,9 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use('/articles', articleRouter);
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
+    res.render('articles/index', { articles: articles });
 });
 
 app.listen(process.env.PORT || 5000);
